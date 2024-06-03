a3aedd65a5c809f72108683463af9cafde43d5a7
Daniel Watson <dkwatson522gmail.com>
2024-06-03 08:07:12 -0400

    Added ingredient nested attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 end
